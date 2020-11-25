1658bbf21e1815524cc3bfbab976fcca26578980
henry sanchez <hrsd83@gmail.com>
2020-11-25 20:24:51 -0300

    Primer commit 'Header'

---
+++ js/script.js
@@ -0,0 +1,32 @@
+  // // Look for .hamburger
+  // var hamburger = document.querySelector(".hamburger");
+  // // On click
+  // hamburger.addEventListener("click", function() {
+  //   // Toggle class "is-active"
+  //   hamburger.classList.toggle("is-active");
+  //   // Do something else, like open/close menu
+  // });
+
+
+
+
+
+// function myFunction() {
+//   let x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+
+function menu(){
+  let menu = document.querySelector('#menu');
+  menu.addEventListener('click', desplegar)
+}
+
+function desplegar(){
+  if(desplegar === true){
+    
+  }
+}
