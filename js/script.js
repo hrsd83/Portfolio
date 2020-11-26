c65a075b554c3ab3238cf868c4c7d47fe638eee5
henry sanchez <hrsd83@gmail.com>
2020-11-26 20:10:37 -0300

    actualizando header

--- js/script.js
+++ js/script.js
@@ -1,32 +1,17 @@
-  // // Look for .hamburger
-  // var hamburger = document.querySelector(".hamburger");
-  // // On click
-  // hamburger.addEventListener("click", function() {
-  //   // Toggle class "is-active"
-  //   hamburger.classList.toggle("is-active");
-  //   // Do something else, like open/close menu
-  // });
 
+//SCRIPT DEL TOGGLE
+let menuToggle = document.querySelector('.menu-toggle');
+let menuToggleIcon = document.querySelector('.menu-toggleA');
+let menu = document.getElementById('menu');
 
+menuToggle.addEventListener('click', e=>{
+  menu.classList.toggle('show');
 
+  // if(menu.classList.contains('show')){
+  //   menuToggleIcon.setAttribute('class','bi-x-square');
 
+  // }else{
+  //   menuToggleIcon.setAttribute('class','.menu-x');
+  // }
+})
 
-// function myFunction() {
-//   let x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
-
-function menu(){
-  let menu = document.querySelector('#menu');
-  menu.addEventListener('click', desplegar)
-}
-
-function desplegar(){
-  if(desplegar === true){
-    
-  }
-}
